419018db8fa47c8e0d3cf546222e0b0b77791292
cecil <cecilghimire@gmail.com>
2022-09-07 22:03:56 +0545

    added store

--- src/App.js
+++ src/App.js
@@ -1,23 +1,44 @@
 import logo from './logo.svg';
 import './App.css';
+import { configureStore } from '@reduxjs/toolkit'
+
+//action types
+export const BUY_NOW='buy-car'
+export const SELL_NOW='sell-car'
+
+
+//reducer
+const initialState={cost:0,quantity:0}
+const  reducer1=(initialState,action)=>{
+   switch(action.type){
+   case BUY_NOW:{
+    return {
+      ...state,
+      cost: initialState.cost+100000,
+      quantity:initialState.quantity+1
+    };
+  }
+  case SELL_NOW:{
+    return {
+      ...state,
+      cost:  initialState.cost-100000,
+      quantity: initialState.quantity-1
+
+    }
+  }
+   } 
+}
+
+//store
+const store=configureStore({
+  reducer:reducer1,
+})
 
 function App() {
+  
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      
     </div>
   );
 }
